80170d87acbfb528161d87f77d9e01cfe168c52e
EC2 Default User <ec2-user@ip-172-31-39-98.ap-northeast-1.compute.internal>
2019-04-14 17:39:05 +0000

    add line-bot code

--- db/migrate/20190414063747_create_users.rb
+++ db/migrate/20190414063747_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :line_id
+      t.string :line_id, null: false # この行を追加
 
       t.timestamps
     end
